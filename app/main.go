14f195902c05b7de449345eb3059a383fe2e83f3
ryichk <ryichk@gmail.com>
2022-04-18 22:51:27 +0900

    :sparkles: Added web server

--- app/main.go
+++ app/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello golang on docker")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+			  <head>
+					<title>go web chat</title>
+				</head>
+				<body>
+					Let's chat!
+				</body>
+			</html>
+		`))
+	})
+	// start web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServer:", err)
+	}
 }
