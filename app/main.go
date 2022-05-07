45f8ecada5a6b1c51eed95e2d4ba7e1a86dbceb3
ryichk <ryichk@gmail.com>
2022-05-07 20:11:17 +0900

    :sparkles: Enabled to specify port.

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -21,16 +22,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	// flag.Stringは、*string型の値を返す。つまり、フラグの値が保持されているアドレスを返す。
+	addr := flag.String("addr", ":8080", "アプリのアドレス")
+	// フラグを解釈する
+	// コマンドラインで指定された文字列から*addrに情報をセット
+	flag.Parse()
+
 	r := newRoom()
 	// route
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
 	// start web server
+	log.Println("webサーバを開始します。ポート: ", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
