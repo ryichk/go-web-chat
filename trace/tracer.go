08cb3b52d244873a0ecb95e2624d0d91d18a22c7
ryichk <ryichk@gmail.com>
2022-05-08 12:48:13 +0900

    :sparkles: Added trace off function

--- trace/tracer.go
+++ trace/tracer.go
@@ -22,3 +22,12 @@ func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
 }
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Traceメソッドの呼び出しを無視するTracerを返す
+func Off() Tracer {
+	return &nilTracer{}
+}
